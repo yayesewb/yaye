43e764859ee32d36acb806f40f1fcacf77e6a45b
yayesewb <yayesewbirhan2011@gmail.com>
2021-08-06 09:38:52 -0900

    updated

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+// function myFunction() {
+//     var x = document.getElementById("menu");
+//     if (x.className === "max-width") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "max-width";
+//     }
+//   }
 
 
 var navbarEle = document.getElementById("nabvar-id");
@@ -11,6 +19,21 @@ window.onscroll = () => {
         navbarEle.style.backgroundColor = "transparent";
     }
 }
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
 
 readContent = document.getElementById("read");
 readContentToggler = document.getElementById("readmorelesetoggler");
